004f46f2a3c91ce05fae7b8895e794e818e2af8c
Pyrdee <liimatainen.pyry@gmail.com>
2021-04-14 23:53:08 +0300

    Luotu sivullesiirtymisnappi ensimmäiselle sivulle. Luotu rekisteröintisivun widgetit.

--- app/src/main/java/com/example/your_week/MainActivity.java
+++ app/src/main/java/com/example/your_week/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.your_week;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void moveToRegistration(View view){
+        Intent move = new Intent(this, Registration.class);
+        startActivity(move);
     }
 }
--- app/src/main/java/com/example/your_week/Registration.java
+++ app/src/main/java/com/example/your_week/Registration.java
@@ -2,13 +2,20 @@ package com.example.your_week;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registration extends AppCompatActivity {
+
+    //public static final String "EXTRA MESSAGE" = "com.example.your_Week.NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+    public void sendInfo(View view){
+       // Intent person = new Intent(this, )
     }
 }
