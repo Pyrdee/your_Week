3658965f92b1ed16eeb604107b62c4f2cb54fc50
aseel <aseelh@metropolia.fi>
2021-04-19 01:07:13 +0300

    Tietojen printtaus tehty ja luotu uusi luokka TIEDOT.java

--- app/src/main/java/com/example/your_week/Registration.java
+++ app/src/main/java/com/example/your_week/Registration.java
@@ -5,25 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
 
+    public static final String EXTRA_TEXT = "name";
+    public static final String EXTRA_TEXT1 = "age";
+    public static final String EXTRA_TEXT2 = "email";
 
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
-    }
-    public void getval(View view){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_registration);
+}
+    public void SendValues(View view) {
+
+
         EditText editText = (EditText) findViewById(R.id.ptName);
         EditText editText1 = (EditText) findViewById(R.id.ptAge);
         EditText editText2 = (EditText) findViewById(R.id.ptEmail);
 
-        String text = editText.getText().toString();
 
-        Intent intent = new Intent (this, TIEDOT.class);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
+        String text = editText.getText().toString();
+        String text1 = editText1.getText().toString();
+        String text2 = editText2.getText().toString();
+
+        Intent intent = new Intent(this, TIEDOT.class);
+
+        intent.putExtra(EXTRA_TEXT, text);
+        intent.putExtra(EXTRA_TEXT1, text1);
+        intent.putExtra(EXTRA_TEXT2, text2);
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/your_week/TIEDOT.java
+++ app/src/main/java/com/example/your_week/TIEDOT.java
@@ -8,29 +8,29 @@ import android.widget.TextView;
 
 public class TIEDOT extends AppCompatActivity {
 
+
 @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_t_i_e_d_o_t);
-        Intent intent = getIntent();
-        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
-    String text1 = intent.getStringExtra(Intent.EXTRA_TEXT);
-    String text2 = intent.getStringExtra(Intent.EXTRA_TEXT);
-
-    TextView textView = (TextView) findViewById(R.id.ptUserName);
-    TextView tView = (TextView) findViewById(R.id.ptUserAge);
-    TextView teView = (TextView) findViewById(R.id.ptUserEmail);
-
-    textView.setText(text);
-    tView.setText(text1);
-    teView.setText(text2);
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_t_i_e_d_o_t);
 
 
+    TextView text =(TextView) findViewById(R.id.ptUserName);
+    TextView tex =(TextView) findViewById(R.id.ptUserAge);
+    TextView te =(TextView) findViewById(R.id.ptUserEmail);
+
+    Intent actIntent = getIntent();
+
+    String name = actIntent.getExtras().getString(Registration.EXTRA_TEXT);
+    String age = actIntent.getExtras().getString(Registration.EXTRA_TEXT1);
+    String Email = actIntent.getExtras().getString(Registration.EXTRA_TEXT2);
+
+    text.setText(name);
+    tex.setText(age);
+    te.setText(Email);
 
 
-
-
-        }
     }
+}
 
