80d8810ab2dc73e89794df7328917123d614f80b
Pyrdee <liimatainen.pyry@gmail.com>
2021-04-21 13:53:27 +0300

    Laitettu actionbar jokaiselle sivulle. Tehty action baariin toimivat napit

--- app/src/main/java/com/example/your_week/MainActivity.java
+++ app/src/main/java/com/example/your_week/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView calendar = findViewById(R.id.bt_calendar);
+        ImageView newTask = findViewById(R.id.bt_newTask);
+        ImageView myProfile = findViewById(R.id.bt_myProfile);
+
+        calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, List_Of_Activities.class);
+                startActivity(intent);
+            }
+        });
+
+        newTask.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewTask.class);
+                startActivity(intent);
+            }
+        });
+        myProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TIEDOT.class);
+                startActivity(intent);
+            }
+        });
+
     }
     public void moveToRegistration(View view){
         Intent move = new Intent(this, Registration.class);
